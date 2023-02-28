47ee90bc21c8adbb4b65a675904ae9580498f493
orangefield <jsw_777@naver.com>
2023-02-28 17:44:05 +0900

    Initial Commit - DB 연결 아직 확인X

---
+++ src/main/java/site/orangefield/teskan_back_practice/TeskanBackPracticeApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.teskan_back_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeskanBackPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeskanBackPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/orangefield/teskan_back_practice/TeskanBackPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.teskan_back_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeskanBackPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
