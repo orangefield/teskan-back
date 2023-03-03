32ea5ab62dd86295cc2c06971fd80376de0f83aa
orangefield <jsw_777>
2023-03-03 15:23:29 +0900

    dto로 리턴 타입 변경 완료

--- src/main/java/site/orangefield/teskan_back_practice/service/TS_EmpService.java
+++ src/main/java/site/orangefield/teskan_back_practice/service/TS_EmpService.java
@@ -1,5 +1,6 @@
 package site.orangefield.teskan_back_practice.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,19 +8,31 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.orangefield.teskan_back_practice.domain.ts_emp.TS_Emp;
 import site.orangefield.teskan_back_practice.domain.ts_emp.TS_EmpRepository;
+import site.orangefield.teskan_back_practice.web.dto.TS_EmpDto;
 
 @RequiredArgsConstructor
 @Service
 public class TS_EmpService {
-    
+
     // DI
     private final TS_EmpRepository ts_EmpRepository;
 
     // emp 목록 조회하기
-    public List<TS_Emp> getEmpList(){
-        List<TS_Emp> empEntity = ts_EmpRepository.findAll();
+    public List<TS_EmpDto> getEmpList() {
+        List<TS_Emp> empsEntity = ts_EmpRepository.findAll();
 
-        return empEntity;
+        List<TS_EmpDto> ts_EmpDto = new ArrayList<>();
+        for (TS_Emp empEntity : empsEntity) {
+            ts_EmpDto.add(TS_EmpDto.builder()
+                    .emp_no(empEntity.getEmp_no())
+                    .name(empEntity.getName())
+                    .position_code(empEntity.getPosition_code())
+                    .dept_code(empEntity.getDept_code())
+                    .tel(empEntity.getTel())
+                    .build());
+        }
+
+        return ts_EmpDto;
     }
 
 }
--- src/main/java/site/orangefield/teskan_back_practice/web/TS_EmpController.java
+++ src/main/java/site/orangefield/teskan_back_practice/web/TS_EmpController.java
@@ -2,12 +2,14 @@ package site.orangefield.teskan_back_practice.web;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import site.orangefield.teskan_back_practice.domain.ts_emp.TS_Emp;
 import site.orangefield.teskan_back_practice.service.TS_EmpService;
+import site.orangefield.teskan_back_practice.web.dto.TS_EmpDto;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +20,7 @@ public class TS_EmpController {
 
     // emp 목록 조회하기
     @GetMapping("/emp/list")
-    public List<TS_Emp> empList(){
+    public List<TS_EmpDto> empList(){
 
         return ts_EmpService.getEmpList();
     }
